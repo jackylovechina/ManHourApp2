24447269e355c40faf01329e1fdb044f12e2bddb
jackylovechina <jackylovechina@gmail.com>
2016-11-21 09:48:44 +0800

    工时管理软件手机端appv2,完成调用日期控件选择日期，并显示在开始时间和结束时间控件上，未后续进行时间筛选做准备

--- app/src/main/java/com/shg/manhourapp2/dialog/DateDialog.java
+++ app/src/main/java/com/shg/manhourapp2/dialog/DateDialog.java
@@ -5,15 +5,24 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.shg.manhourapp2.R;
+
+import java.util.Calendar;
+
+import static android.R.attr.y;
+import static com.shg.manhourapp2.R.id.day;
+import static com.shg.manhourapp2.R.id.month;
 
 /**
  * Created by Administrator on 2016/11/18 0018.
  */
 
-public class DateDialog extends DialogFragment {
+public class DateDialog extends DialogFragment implements DatePicker.OnDateChangedListener {
+
+    private DatePicker datePicker_DP;
     private TextView textView;
     private int whatDate;
 
@@ -26,6 +35,9 @@ public class DateDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+        datePicker_DP = (DatePicker) view.findViewById(R.id.dp_date_picker);
+
+        initDatePicker();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         switch (whatDate) {
@@ -46,5 +58,25 @@ public class DateDialog extends DialogFragment {
 
 
         return builder.create();
+    }
+
+    private void initDatePicker() {
+
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int monthOfYear = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int month = monthOfYear + 1;
+        textView.setText(year + "-" + month + "-" + dayOfMonth);
+        datePicker_DP.init(year, monthOfYear, dayOfMonth, this);
+
+    }
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+        int month = monthOfYear + 1;
+        textView.setText(year + "-" + month + "-" + dayOfMonth);
     }
 }
