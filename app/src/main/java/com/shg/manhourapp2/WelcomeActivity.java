bb67e29b3842e244c16f798084416eb376f17325
jackylovechina <jackylovechina@gmail.com>
2016-11-29 16:40:59 +0800

    工时管理软件手机端appv2,完成载入界面倒计时显示功能，完成欢迎载入界面到登录界面的跳转功能

--- app/src/main/java/com/shg/manhourapp2/WelcomeActivity.java
+++ app/src/main/java/com/shg/manhourapp2/WelcomeActivity.java
@@ -1,8 +1,15 @@
 package com.shg.manhourapp2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Administrator on 2016/11/29 0029.
@@ -10,9 +17,59 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private TextView mTV_welcomeNum;
+
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            mTV_welcomeNum.setText(msg.what + "s");
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        mTV_welcomeNum = (TextView) findViewById(R.id.tv_welcomeNum);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 2; i >= 0; i--) {
+
+                    try {
+                        Thread.sleep(1000);
+                        Message msg = new Message();
+                        msg.what = i;
+
+                        handler.sendEmptyMessage(msg.what);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent();
+                intent.setClass(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+
+            }
+        };
+
+        timer.schedule(timerTask, 3000);
     }
+
 }
