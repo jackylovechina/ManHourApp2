5132ae0d3b6f92f685d1c4ca3dab060df0edbe79
jackylovechina <jackylovechina@gmail.com>
2016-11-29 15:04:49 +0800

    工时管理软件手机端appv2,完成欢迎载入和缓冲界面的布局，

---
+++ app/src/main/java/com/shg/manhourapp2/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.shg.manhourapp2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/11/29 0029.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
