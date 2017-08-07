f14dad83b88cfcbb171b2ee0627bc199e8346772
jackylovechina <jackylovechina@gmail.com>
2017-08-07 13:58:15 +0800

    美化用户信息显示界面，完善UI

--- app/src/main/java/com/shg/manhourapp2/dialog/UserInfoDialog.java
+++ app/src/main/java/com/shg/manhourapp2/dialog/UserInfoDialog.java
@@ -16,14 +16,18 @@ import com.shg.manhourapp2.utils.GlobalVar;
 
 public class UserInfoDialog extends DialogFragment {
     private TextView userName_TV;
+    private TextView userID_TV;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("个人信息");
+        builder.setIcon(R.drawable.id_card);
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_userinfo, null);
         userName_TV = (TextView) view.findViewById(R.id.tv_userInfo_name);
+        userID_TV= (TextView) view.findViewById(R.id.tv_userInfo_id);
         userName_TV.setText(GlobalVar.sysUser.employeeViewModel.name);
+        userID_TV.setText(GlobalVar.sysUser.employeeViewModel.num);
         builder.setView(view);
 
         return builder.create();
